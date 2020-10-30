6d1883bb0da1740e7c35108acc723d39b87b6f6f
Erablast <maxime50maxtime@gmail.com>
2020-10-30 16:57:17 +0100

    Mise en page de la page produit

--- javascript.js
+++ javascript.js
@@ -1,44 +1,62 @@
 var request = new XMLHttpRequest();
 
 request.onreadystatechange = function() {
-    let teddyImage = document.getElementsByClassName("teddyImage").item(0);
-    let teddyName = document.getElementsByClassName("teddyName").item(0);
-    let teddyPrice = document.getElementsByClassName("teddyPrice").item(0);
-    let teddyColors = document.getElementsByClassName("teddyColors").item(0);
-    let teddyDesc = document.getElementsByClassName("teddyDesc").item(0);
-
-    let teddyImage1 = document.getElementsByClassName("teddyImage").item(1);
-    let teddyName1 = document.getElementsByClassName("teddyName").item(1);
-    let teddyPrice1 = document.getElementsByClassName("teddyPrice").item(1);
-    let teddyColors1 = document.getElementsByClassName("teddyColors").item(1);
-    let teddyDesc1 = document.getElementsByClassName("teddyDesc").item(1);
-
-    let teddyImage2 = document.getElementsByClassName("teddyImage").item(2);
-    let teddyName2 = document.getElementsByClassName("teddyName").item(2);
-    let teddyPrice2 = document.getElementsByClassName("teddyPrice").item(2);
-    let teddyColors2 = document.getElementsByClassName("teddyColors").item(2);
-    let teddyDesc2 = document.getElementsByClassName("teddyDesc").item(2);
     
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
         var response = JSON.parse(this.responseText);
         console.log(response)
-        teddyImage.src = response[0].imageUrl;
-        teddyName.innerHTML = response[0].name;
-        teddyPrice.innerHTML = response[0].price + " €";
-        teddyColors.innerHTML = response[0].colors;
-        teddyDesc.innerHTML = response[0].description;
 
-        teddyImage1.src = response[1].imageUrl;
-        teddyName1.innerHTML = response[1].name;
-        teddyPrice1.innerHTML = response[1].price + " €";
-        teddyColors1.innerHTML = response[1].colors;
-        teddyDesc1.innerHTML = response[1].description;
+        response.forEach(injection)
 
-        teddyImage2.src = response[2].imageUrl;
-        teddyName2.innerHTML = response[2].name;
-        teddyPrice2.innerHTML = response[2].price + " €";
-        teddyColors2.innerHTML = response[2].colors;
-        teddyDesc2.innerHTML = response[2].description;
+        function injection(response){
+            let parentDiv = document.getElementById("javascript__injection")
+
+            let productList_wrapper = document.createElement("div");
+            productList_wrapper.className = ("productList_wrapper");
+
+            let productList_wrapper_left = document.createElement("div");
+            productList_wrapper_left.className = ("productList_wrapper_left");
+
+            let productImage = document.createElement("img");
+            productImage.src = response.imageUrl;
+            productImage.className = "productPreview teddyImage";
+
+            let productList_wrapper_right = document.createElement("div");
+            productList_wrapper_right.className = ("product_wrapper_right");
+
+            let productText = document.createElement("div");
+            productText.className = "productText";
+            
+            var productTitle = document.createElement('h1');
+            productTitle.innerHTML = response.name;
+            productTitle.className = 'productTitle underline font teddyName';
+
+            var productPrice = document.createElement('h2');
+            productPrice.innerHTML = response.price + " €";
+            productPrice.className = 'productPrice underline font teddyPrice';
+
+            var productColors = document.createElement('p');
+            productColors.innerHTML = response.colors;
+            productColors.className = 'productColors underline font teddyColors';
+
+            var productDescription = document.createElement('p');
+            productDescription.innerHTML = response.description;
+            productDescription.className = 'productDescription underline font teddyDesc';
+ 
+            parentDiv.appendChild(productList_wrapper);
+            productList_wrapper.appendChild(productList_wrapper_left)
+            productList_wrapper.appendChild(productList_wrapper_right)
+            productList_wrapper_left.appendChild(productImage);
+            productList_wrapper_right.appendChild(productText);
+            productText.appendChild(productTitle);
+            productText.appendChild(productPrice);
+            productText.appendChild(productColors);
+            productText.appendChild(productDescription);
+
+        }
+
+
+
         
     }
 };
